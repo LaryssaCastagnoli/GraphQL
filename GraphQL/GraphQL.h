5fd0cdf8a227b3b32c2d58f9540fb5a4537b910d
Laryssa Castagnoli <laryssafragoso@brq.com>
2019-01-09 11:33:21 -0200

    Initial Commit

---
+++ GraphQL/GraphQL.h
@@ -0,0 +1,19 @@
+//
+//  GraphQL.h
+//  GraphQL
+//
+//  Created by BRQ on 09/01/19.
+//  Copyright © 2019 Solid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GraphQL.
+FOUNDATION_EXPORT double GraphQLVersionNumber;
+
+//! Project version string for GraphQL.
+FOUNDATION_EXPORT const unsigned char GraphQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraphQL/PublicHeader.h>
+
+
